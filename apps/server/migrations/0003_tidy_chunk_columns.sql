66ee25d0d08a438de5c4ba4a05b23f2489326699
dkirby-ms <saitcho@outlook.com>
2026-07-22 13:21:51 -0500

    chore(ci): migration fix

--- apps/server/migrations/0003_tidy_chunk_columns.sql
+++ apps/server/migrations/0003_tidy_chunk_columns.sql
@@ -1,7 +1,7 @@
-ALTER TABLE "tiles" ADD COLUMN "chunk_x" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "tiles" ADD COLUMN "chunk_y" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "tiles" ADD COLUMN IF NOT EXISTS "chunk_x" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "tiles" ADD COLUMN IF NOT EXISTS "chunk_y" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
 UPDATE "tiles"
 SET
 	"chunk_x" = floor("pos_x" / 8.0)::integer,
 	"chunk_y" = floor("pos_y" / 8.0)::integer;--> statement-breakpoint
-CREATE INDEX "tiles_canvas_chunk_created_idx" ON "tiles" USING btree ("canvas_id", "chunk_x", "chunk_y", "created_at");
+CREATE INDEX IF NOT EXISTS "tiles_canvas_chunk_created_idx" ON "tiles" USING btree ("canvas_id", "chunk_x", "chunk_y", "created_at");
